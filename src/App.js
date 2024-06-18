43671f20121e9b903687b596a5031a03665bd0c8
Samuel Annan <samuelannan191@gmail.com>
2024-06-18 11:50:40 -0700

    feat(create,read)

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Index from './components';
+;
+
+function App() {
+  return (
+    <div className="App">
+      <Index/>
+
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState } from 'react'
+
+function Index() {
+  const [todoList, setTodoList] = useState ([]);
+  const [newTask, setNewTask] = useState ("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+
+  };
+ 
+  const addTask = () =>{
+    const NewTodoList = [...todoList, newTask]
+    setTodoList(NewTodoList);
+
+  };
+  return (
+
+
+    <div>
+    <div className='addTask'></div>
+     <input onChange={handleChange}/>
+     <button onClick={addTask}> Add Task</button>
+
+    <div className='list'>
+    {todoList.map((task,index) => {
+      return <h1>{task}</h1>
+    })}
+    </div>
+    </div>
+  )
+}
+
+export default Index
+
