49c0e8b20bd15904d5f7682ab1726c32933f7c89
Samuel Annan <ce-sannan5821@st.umat.edu.gh>
2024-07-02 00:22:10 +0000

    advanced interface

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,45 +1,36 @@
-import React from 'react';
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 function Index() {
-  const [todoList, setTodoList] = useState ([]);
-  const [newTask, setNewTask] = useState ("");
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
   const handleChange = (event) => {
     setNewTask(event.target.value);
-
   };
- 
-  const addTask = () =>{
-    const task = {
-      id :todoList.length=== 0 ? 1 : todoList[todoList.length -1].id + 1 ,
-      taskName : newTask,
-    };
-    
-    setTodoList([...todoList, task]);
-    
 
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      const task = {
+        id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+        taskName: newTask,
+      };
+
+      setTodoList([...todoList, task]);
+      setNewTask(''); // Clear the input field after adding the task
+    }
   };
-  
-  const deleteTask = (id) =>{
-    const newTodoList = todoList.filter((task) =>{
-      if (task.id === id){
-        return false;
-      } else {
-       return true;
-      }
-     });
+
+  const deleteTask = (id) => {
+    const newTodoList = todoList.filter((task) => task.id !== id);
     setTodoList(newTodoList);
-  }; 
- 
- 
+  };
 
   const complete = (selectedTaskName) => {
     const strikeThrough = todoList.map((task) => {
       if (task.taskName === selectedTaskName) {
         return {
           ...task,
-          taskName: <strike className >{task.taskName}</strike>,
+          taskName: <span style={{ textDecoration: 'line-through' }}>{task.taskName}</span>,
         };
       } else {
         return task;
@@ -47,35 +38,24 @@ function Index() {
     });
     setTodoList(strikeThrough);
   };
-  
-
-
 
   return (
-
-
     <div>
-    <div className='addTask'>
-     <input onChange={handleChange}/>
-     <button className='taskButton' onClick={addTask}> Add Task</button>
-     </div>
-    <div className='list'>
-    {todoList.map((task,index) => {
-      return(
-        <div className='x--button'>
-        
-        <h1>{task.taskName}</h1>
-       
-        <button onClick={() => deleteTask (task.id)}> X </button>
-        <button onClick={() => complete(task.taskName)} className='button5'>Completed</button>
-        </div>
-      );
-    })}
-
+      <div className='addTask'>
+        <input value={newTask} onChange={handleChange} />
+        <button className='taskButton' onClick={addTask}>Add Task</button>
+      </div>
+      <div className='list'>
+        {todoList.map((task) => (
+          <div key={task.id} className='x--button'>
+            <h1>{task.taskName}</h1>
+            <button onClick={() => deleteTask(task.id)}>X</button>
+            <button onClick={() => complete(task.taskName)} className='button5'>Completed</button>
+          </div>
+        ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Index
-
+export default Index;
